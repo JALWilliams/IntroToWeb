b63d54fdc213f4a32b987b9b14dfe7b3e744fb8c
JALWilliams <just.williams04@gmail.com>
2022-01-10 16:33:18 +0000

    intro to params

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/justine' do
   "hii"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Bianca", "Jerry", "Misty"].sample 
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
